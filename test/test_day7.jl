8a7a2af3c29b4778cf631989304acdbeaf938c87
Brynjar Smári Bjarnason <binni@binnisb.com>
2020-12-13 22:13:08 +0100

    rm test

--- test/test_day7.jl
+++ test/test_day7.jl
@@ -3,10 +3,8 @@
     # Write your tests here.
     lines = read_infile(Day7_1, "$(@__DIR__)/assets/day7_test1.txt")
     @test solve(Day7_1, lines) == 4
+
     @test solve(Day7_2, lines) == 32
-
-
-    @test AdventOfCode.parents()
     
     lines = read_infile(Day7_1, "$(@__DIR__)/assets/day7_test2.txt")
     @test solve(Day7_2, lines) == 126
